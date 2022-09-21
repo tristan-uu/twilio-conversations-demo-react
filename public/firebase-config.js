0ebb216d84e2c068ad55c2b584a95056ad0583ea
tristan-uu <112981584+tristan-uu@users.noreply.github.com>
2022-09-21 14:58:51 -0400

    Create firebase-config.js

---
+++ public/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCiGxYRcJl323XUd8Vx5Eve-_4xMGv-DYs",
+    authDomain: "fir-twilio-87f47.firebaseapp.com",
+    projectId: "fir-twilio-87f47",
+    storageBucket: "fir-twilio-87f47.appspot.com",
+    messagingSenderId: "505037476822",
+    appId: "1:505037476822:web:7191ddff523f440660ca77",
+    measurementId: "G-C6Q0TH4M6W"
+  };
